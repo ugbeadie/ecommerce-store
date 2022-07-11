b5b3b92ac04b66975fe4a8147318c9036f7209e9
ugbeadie <ugbeadie3@gmail.com>
2022-07-12 00:24:42 +0100

    major changes

--- script.js
+++ script.js
@@ -147,7 +147,13 @@ function purchased() {
     updateTotal()
 }
 
-
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }, 
+});
 
 
 
